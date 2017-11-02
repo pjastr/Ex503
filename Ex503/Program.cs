fd5d378d41c9bd95645b0bdb12721d5319743d96
Piotrek <@>
2017-11-02 22:52:02 +0100

    przesłanie plików

---
+++ Ex503/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ex503
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ArrayList lista = new ArrayList();
+            lista.Add(new Temperatura(21.1));
+            lista.Add(new Temperatura(-3));
+            lista.Add(new Temperatura(12.8));
+            lista.Add(new Temperatura(-5.3));
+            lista.Sort();
+            foreach(var element in lista)
+            {
+                Console.WriteLine(element.ToString());
+            }
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Ex503/Temperatura.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ex503
+{
+    class Temperatura : IComparable
+    {
+        private double wartosc;
+
+        public Temperatura(double wart) //konstruktor
+        {
+            this.wartosc = wart;
+        }
+
+        public int CompareTo(Object obj)
+        {
+            if (obj == null) return 1;
+
+            Temperatura other = obj as Temperatura;  //traktowanie obj jako typ Temperatura
+            if (other != null)
+            {
+                if (other.wartosc < this.wartosc)
+                {
+                    return 1;
+                }
+                else if (other.wartosc == this.wartosc)
+                {
+                    return 0;
+                }
+                else
+                {
+                    return -1;
+                }
+            }
+            else
+                throw new ArgumentException("Objekt nie jest temperaturą"); //wyrzucenie wyjątku
+        }
+
+        public override string ToString()  //przesłonięcie metody (aby można było wypisać na konsoli)
+        {
+
+            return this.wartosc.ToString();
+        }
+    }
+}
